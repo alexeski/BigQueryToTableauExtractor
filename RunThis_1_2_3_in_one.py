91e114b46f4f11e1cf23366acb894fbabe9593ad
alexeskinasy <8482762+alexeskinasy@users.noreply.github.com>
2019-06-27 16:17:39 +0000

    Update RunThis_1_2_3_in_one.py

--- RunThis_1_2_3_in_one.py
+++ RunThis_1_2_3_in_one.py
@@ -3,15 +3,15 @@ from timeit import default_timer as timer
 
 # Export data from Big Query to Bucket
 start = timer()
-os.system("python export_data.py")
+os.system("python 1_export_data.py")
 end = timer()
 print("Timer: " + str(round((end-start)/60,3)) + " minutes")
 
 # Download data from Google Bucket to local drive
 start = timer()
-os.system("python download_bucket_files.py")
+os.system("python 2_download_bucket_files.py")
 end = timer()
 print("Timer: " + str(round((end-start)/60,3)) + " minutes")
 
 # Trigger Extract refresh (asyncronous)
-os.system("python refreshExtractByName.py -s <your_Tableau_Server_URL_here> -u <your_admin_username> -pw <your_admin_password> -dn \"<your_unique_datasource_name_here>\" ")
+os.system("python 3_refreshExtractByName.py -s <your_Tableau_Server_URL_here> -u <your_admin_username> -pw <your_admin_password> -dn \"<your_unique_datasource_name_here>\" ")
